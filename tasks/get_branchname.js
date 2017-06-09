18c0c3624f69beeef7ddd4a7beeb0c5e8a17109b
dastangoo <hamed.dastangoo@gmail.com>
2017-06-09 13:21:30 +0430

    Just a commit

--- tasks/get_branchname.js
+++ tasks/get_branchname.js
@@ -1,4 +1,4 @@
-/*
+ /*
  * grunt-get-branchname
  * https://github.com/dastangoo/grunt-get-branchname
  *
@@ -15,35 +15,21 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('get_branchname', 'Get current branch name and pass it to another task\'s config', function() {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    var done = this.async(),
+        options = this.options({
+          punctuation: '.',
+          separator: ', '
+        });
+    grunt.util.spawn({
+      cmd: 'git',
+      args: ['symbolic-ref', 'HEAD', '--short']
+    }, function (error, result) {
+      if (error) {
+        grunt.log.error([error]);
+        done(false);
+      }
+      grunt.config.set('gitrebase.dist.options.branch', result.stdout);
+      done();
     });
   });
 
